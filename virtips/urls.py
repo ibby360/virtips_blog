31c020e2dbb7f956c2c8527d6e1dd589b4f4e089
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-07 22:42:48 +0300

    Include CKEditor URL

--- virtips/urls.py
+++ virtips/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'ckeditor/', include('ckeditor_uploader.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
