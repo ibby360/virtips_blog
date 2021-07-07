0e479fc507d344842b79af17b3fb9448bc9a5d98
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-07 17:02:49 +0300

    Add post and author models to the admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from blog.models import Post, Author
 
 # Register your models here.
+admin.site.register(Author)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
