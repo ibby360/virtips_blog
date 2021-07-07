1e233598a544632cfce01685147bf6651814d935
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-07 16:20:23 +0300

    Configure templates location

--- virtips/settings.py
+++ virtips/settings.py
@@ -55,10 +55,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'virtips.urls'
 
+# Templates Directory
+TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
