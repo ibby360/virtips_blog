dc78a4f8b62d1c1553f8a3584290ac26fb0b307d
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-07 17:03:26 +0300

    Author and Post Models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+class Author(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.author.username
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(Author, related_name='blog_post', on_delete=models.CASCADE)
+    body = RichTextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('-publish',)
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    def __str__(self):
+        return self.title
