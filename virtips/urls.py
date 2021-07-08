7be28a78926475877b457a15df2dfc5ba0f3fd6f
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-08 22:21:42 +0300

    Include blog app urls

--- virtips/urls.py
+++ virtips/urls.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from virtips import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'ckeditor/', include('ckeditor_uploader.urls')),
-]
+    path('', views.home, name='home'),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('blog/', include('blog.urls')),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
