885431e0dcbdf33988f461fe2b5abc422a90e884
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-08 22:20:02 +0300

    Post view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 # Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/blog.html', context)
+
+def post_detail(request, post):
+    post = get_object_or_404(Post, slug=post, status='published')
+    context = {
+        'post': post
+    }
+    return render(request, '', context)
