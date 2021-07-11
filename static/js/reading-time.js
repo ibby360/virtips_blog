12d68b5da85bf6688138b650e907a17003008c55
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-11 20:48:45 +0300

    Calculate the estimated reading time of an article

---
+++ static/js/reading-time.js
@@ -0,0 +1,8 @@
+function readingTime() {
+  const text = document.getElementById("article").innerText;
+  const wpm = 225;
+  const words = text.trim().split(/\s+/).length;
+  const time = Math.ceil(words / wpm);
+  document.getElementById("time").innerText = time;
+}
+readingTime();
