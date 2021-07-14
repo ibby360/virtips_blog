d1e13b4058b49a6e9b6aac2e32cc50d52033965a
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-14 21:52:02 +0300

    Category model

---
+++ category/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse('blog:post_by_category', args=[self.slug])
+
+    def __str__(self):
+        return self.category_name
