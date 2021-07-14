3a34c85eebcbc9a66b93cbf8d53f568bdd3438d8
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-14 16:48:25 +0300

    Render post by category

--- blog/views.py
+++ blog/views.py
@@ -2,39 +2,46 @@ from django.urls import reverse
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect 
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.core.exceptions import ObjectDoesNotExist
+from category.models import Category
 from blog.models import Post, Author
 from django.db.models import Q
 from taggit.models import Tag
 
 # Create your views here.
 
-def post_list(request, tag_slug=None):
+def post_list(request, tag_slug=None, category_slug=None): 
+    categories = None
+    posts = None
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        posts = Post.published.filter(category=categories)
+        paginator = Paginator(posts, 9)
+        page = request.GET.get('page')
+        paged_posts = paginator.get_page(page)
+        post_count = posts.count()
+    else:
+        posts = Post.published.all()
+        paginator = Paginator(posts, 9)
+        page = request.GET.get('page')
+        paged_posts = paginator.get_page(page)
+        post_count = posts.count()
+
     tags = Tag.objects.all()
-    posts = Post.published.all()
     tag = None
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
         posts = posts.filter(tags__in=[tag])
-
-    paginator = Paginator(posts, 9)
-    page = request.GET.get('page')
-    paged_posts = paginator.get_page(page)
-    try:
-        paged_posts = paginator.page(page)
-    except PageNotAnInteger:
-        paged_posts = paginator.page(1)
-    except EmptyPage:
-        paged_posts = paginator.page(paginator.num_pages)
-        
+  
     context = {
         'posts': paged_posts,
         'tags': tags,
         'tag': tag,
+        'post_count': post_count
     }
     return render(request, 'blog/blog.html', context)
 
-def post_detail(request, post):
-    post = get_object_or_404(Post, slug=post, status='published')
+def post_detail(request, post, category_slug):
+    post = get_object_or_404(Post, slug=post, status='published', category__slug=category_slug)
     try:
         previous_post = post.get_previous_by_publish()
         next_post = post.get_next_by_publish()
