e055d2fa96aa7f50dec2be99501bbfde4fbece1d
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-16 12:57:28 +0300

    Canonical URLs for Post model

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
-from taggit.managers import TaggableManager 
+from taggit.managers import TaggableManager
+from category.models import Category 
 
 # Create your models here.
 class PublishedManager(models.Manager):
@@ -13,8 +14,11 @@ class PublishedManager(models.Manager):
 
 class Author(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    profile_pic = models.ImageField()
+    profile_pic = models.ImageField(default='static/img/author/default-profile.png')
     description = models.TextField()
+
+    def get_author_url(self):
+        return reverse('author', args=[self.id])
 
     def __str__(self):
         return self.author.username
@@ -26,6 +30,7 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, default='')
     tags = TaggableManager()
     author = models.ForeignKey(Author, related_name='blog_post', on_delete=models.CASCADE)
     thumbnail = models.ImageField(upload_to='post')
@@ -35,10 +40,6 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='draft')
     featured = models.BooleanField(default=False)
-    previous_post = models.ForeignKey(
-        'self', related_name='previous', on_delete=models.SET_NULL, blank=True, null=True)
-    next_post = models.ForeignKey(
-        'self', related_name='next', on_delete=models.SET_NULL, blank=True, null=True)
 
     objects  = models.Manager() # Default Manager
     published = PublishedManager() # Custom Manager
@@ -53,5 +54,5 @@ class Post(models.Model):
 
 
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args=[self.slug])
+        return reverse("blog:post_detail", args=[self.category.slug, self.slug])
     
