c6013360519399208a1e87aa194ff5241d5e2053
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-16 05:57:39 +0300

    Athentication URLs

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from collections import namedtuple
+from django.urls import path
+from accounts import views
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout')
+]
