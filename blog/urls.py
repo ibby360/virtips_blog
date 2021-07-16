7379ff6b51b0193c661bb96a20b12898615a8edb
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-16 12:59:11 +0300

    Display posts by category

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from blog import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list' ),
+    path('category/<slug:category_slug>/', views.post_list, name='post_by_category'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('search/', views.search, name='search'),
-    path('<slug:post>/', views.post_detail, name='post_detail'),
+    path('<slug:category_slug>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
