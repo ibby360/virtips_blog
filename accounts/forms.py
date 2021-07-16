d0752dbad52b299670d8cf012f856811a4d402db
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-16 05:58:27 +0300

    Account registration form

---
+++ accounts/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from accounts.models import Account
+
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+        'class': 'form-control',
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm Password'           
+    }))
+
+    class Meta:
+        model = Account
+        fields = ['username', 'email', 'password']
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password does not match!"
+            )
+
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter Email Address'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
