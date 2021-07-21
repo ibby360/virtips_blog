84f33240c3620fbde80e26ea6de59963dc1a68d3
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-22 00:07:35 +0300

    Add accpunts cativation url

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from accounts import views
 urlpatterns = [
     path('register/', views.register, name='register'),
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout')
 ]
