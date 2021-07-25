8b75f58f387b0a095e16c6f473a6a8915a249e50
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-25 13:25:44 +0300

    Formating the document

--- virtips/views.py
+++ virtips/views.py
@@ -1,24 +1,29 @@
-from django.core import paginator
-from django.shortcuts import render
-from blog.models import Post
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post, Author
+
 
 def home(request):
     featured = Post.published.filter(featured=True)[:2]
-    posts = Post.published.order_by('-publish').exclude(featured=True)
+    posts = Post.published.order_by('-publish').exclude(featured=True)[:9]
     context = {
         'featured': featured,
         'posts': posts
     }
     return render(request, 'index.html', context)
 
+
 def about(request):
     return render(request, 'about.html', {})
+
 
 def contact(request):
     return render(request, 'contact.html', {})
 
+
 def error_404(request, exception):
     return render(request, 'page404.html', {})
+
 
 def error_500(request):
     return render(request, 'page500.html', {})
