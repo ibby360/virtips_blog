95a4d86cd93ed136b7905c344f1fbd1e45349bb9
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-25 13:23:55 +0300

    Add Author view url

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('search/', views.search, name='search'),
     path('<slug:category_slug>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('@<str:username>/', views.author, name='author'),
 ]
