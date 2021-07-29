43f0ecb6e012c5c3a545ee0f264aa13f1128b614
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-30 02:18:55 +0300

    [FEAT]✨ Allow user to reset their password

--- accounts/views.py
+++ accounts/views.py
@@ -48,6 +48,7 @@ def register(request):
         'form': form
     }
     return render(request, 'auth/register.html', context)
+    
 
 @unauthenticated_user
 def login(request):
@@ -68,6 +69,7 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect('login')
+    
 
 @unauthenticated_user
 def activate(request, uidb64, token):
@@ -86,3 +88,68 @@ def activate(request, uidb64, token):
     else:
         messages.error(request, 'Invalid activation link')
         return redirect('register')
+        
+        
+@unauthenticated_user
+def forgot_password(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user = Account.objects.get(email__exact=email)
+
+            # Reset password email
+            current_site = get_current_site(request)
+            mail_subject = 'Welcome to VirtTips! Reset Your Password.'
+            message = render_to_string('auth/reset_password_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+
+            messages.success(request, 'Password reset email has been sent to your email address.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Account does not exist!')
+            return redirect('forgot_password')
+    return render(request, 'auth/forgotPassword.html')
+
+
+@unauthenticated_user
+def resetpassword_validate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.success(request, 'Please reset your password')
+        return redirect('reset_password')
+    else:
+        messages.error(request, 'This link has been expired!')
+        return redirect('login')
+
+        
+@unauthenticated_user
+def reset_password(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            uid = request.session.get('uid')
+            user = Account.objects.get(pk=uid)
+            user.set_password(password)
+            user.save()
+            messages.success(request, 'Password reset successful')
+            return redirect('login')
+        else:
+            messages.error(request, 'Password do not match!')
+            return redirect('resetPassword')
+    else:
+        return render(request, 'auth/resetPassword.html')
