c90f5073e78122350604b84dc19ca63cab7b1202
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-30 02:19:35 +0300

    [FEAT]✨ Allow user to reset their password

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
     path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout')
+    path('logout/', views.logout, name='logout'),
+    path('forgot_password/', views.forgot_password, name='forgot_password'),
+    path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
+    path('reset_password/', views.reset_password, name='reset_password'),
 ]
