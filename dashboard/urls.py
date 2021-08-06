3f921b281ece741754047d3cadd7c35849c056ab
Ibby360 <ibrahimramadhan360@outlook.com>
2021-08-07 00:48:04 +0300

    🚚 create url patterns for dashboard views

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from dashboard import views
 
+app_name = 'dashboard'
 urlpatterns = [
-    path('me/', views.dashboard, name='dashboard')
+    path('me/', views.dashboard, name='dashboard'),
+    path('me/article/create/', views.create_post, name='article-create'),
+    path('me/published-articles/', views.published_articles, name='published-articles'),
+    path('me/drafted-articles/', views.drafted_articles, name='drafted-articles'),
+    path('me/<slug:slug>/', views.post_details, name='post_details'),
+    path('me/edit/<slug:slug>/', views.edit_blog_post, name='edit-post'),
 ]
