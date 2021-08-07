487d15c895ffcb0b2f21c845e34ed9357455eaef
Ibby360 <ibrahimramadhan360@outlook.com>
2021-08-08 00:19:30 +0300

    ✨ Starts signals to create author profile

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    # Starts signals to create author profile.
+    def ready(self):
+        import accounts.signals
