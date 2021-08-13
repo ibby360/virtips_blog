4de09429248cfe1219839b1fd8c653b5b11c4c71
Ibby360 <ibrahimramadhan360@outlook.com>
2021-08-13 22:59:00 +0300

    Custom template tags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -7,24 +7,10 @@ register = template.Library()
 
 
 @register.simple_tag
-def get_most_viewed_postss(count=4):
-    return Post.published.filter(views__gt=8)[:count]
-
-# 
-# def get_most_commented_posts(count=4):
-# 
-    # return Post.published.annotate(
-        # total_comments=Count('comments')
-    # ).filter(publish__range=[timezone.now() - timezone.timedelta(seconds=3), timezone.now()]).order_by('-total_comments')[:count]
+def get_most_viewed_posts(count=4):
+    return Post.published.filter(views__gt=10)[:count]
 
 
 @register.simple_tag
 def get_latest_posts():
     return Post.published.order_by('-publish')[:4]
-
-@register.simple_tag
-def get_most_viewed_posts(count=4):
-    return Post.published.annotate(
-total_views=Count('views')
-).order_by('-total_views')[:count]
-
