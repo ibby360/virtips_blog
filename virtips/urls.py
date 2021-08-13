bec95ad29c2107ccb9dd37dfd4ce8530c2c96f6b
Ibby360 <ibrahimramadhan360@outlook.com>
2021-08-13 23:33:46 +0300

    ✨  Create a sitemap

--- virtips/urls.py
+++ virtips/urls.py
@@ -3,8 +3,14 @@ from django.conf.urls import handler404
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.sitemaps.views import sitemap
+from virtips.sitemaps import PostSitemap
 
 from virtips import views
+
+sitemaps = {
+    'posts': PostSitemap,
+}
 
 handler404 = views.error_404
 handler500 = views.error_500
@@ -17,6 +23,7 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
     path('dashboard/', include('dashboard.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
