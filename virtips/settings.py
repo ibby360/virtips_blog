289bc3f6eff18cabd5efad41888ae1670fb61ee8
Ibby360 <ibrahimramadhan360@outlook.com>
2021-09-07 00:21:12 +0300

    🔒️ Hide sensiitive information from the code

--- virtips/settings.py
+++ virtips/settings.py
@@ -18,10 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7$=b_#ejsh=75_t8an3endm*1v%t!7r58kd0t&8$#rzr1ah@gz'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
